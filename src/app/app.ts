5d3371edb6608ccd541315a6529c0f68c6e39f06
Paula Benalcázar <paulabenalcazar7lp@gmail.com>
2025-09-25 00:10:44 -0500

    Creación de componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import {ContactList} from './components/contact-list/contact-list';
+import {Header} from './components/header/header';
+import {Sidebar} from './components/sidebar/sidebar';
+import { Dashboard } from './components/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ContactList],
+  imports: [ContactList, Header, Sidebar, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
